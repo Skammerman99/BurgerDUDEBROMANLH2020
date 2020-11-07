6c3328d34e70c7b282dd8fa21bea66961b788d57
Spencer Kammerman <spencerkam99@gmail.com>
2020-11-06 20:57:56 -0800

    Populating and wiping champ select
    Includes updates to Vi's champ splash downloader to make it functional for the full 10 champion CS. Uses help.py to clear the images and remake them as transparencies. Also some html stuff that we don't need anymore

--- champion splash.py
+++ champion splash.py
@@ -3,19 +3,24 @@ import shutil
 
 id = 266
 # somewhere here relate champ id to champ name, in this case Nami
-champion = "Vi"
-num = 1
+champions = ["Shen", "Graves", "Syndra", "Samira", "Leona", "Ornn", "Hecarim", "Galio", "Ezreal", "Yuumi"]
+num = 0
+cs_slot_num = 1
 
-image_url= "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/{}_{}.jpg".format(champion, num)
-filename = image_url.split("/")[-1]
-print(filename)
+for champion in champions:
+    image_url= "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{}_{}.jpg".format(champion, num)
+    #filename = image_url.split("/")[-1]
+    filename = "champ" + str(cs_slot_num) + ".png"
+    print(filename)
 
-r= requests.get(image_url, stream =True)
+    r= requests.get(image_url, stream =True)
 
-if r.status_code ==200:
-    r.raw.decode_content = True
-    with open(filename, 'wb') as f:
-        shutil.copyfileobj(r.raw, f)
-    print('Image successfully downloaded: {}'.format(filename))
-else:
-    print("Image could\n't be retrieved")
+    if r.status_code ==200:
+        r.raw.decode_content = True
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+        print('Image successfully downloaded: {}'.format(filename))
+    else:
+        print("Image could\n't be retrieved")
+
+    cs_slot_num += 1
