32741536a7820d7efea03735a182a8d865a61405
vivilett <39755873+vivilett@users.noreply.github.com>
2020-11-07 13:53:26 -0800

    Create champion icons.py

---
+++ champion icons.py
@@ -0,0 +1,18 @@
+import requests
+import shutil
+
+champion = "Yasuo"
+
+image_url= "http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/{}.png".format(champion)
+filename = image_url.split("/")[-1]
+print(filename)
+
+r= requests.get(image_url, stream =True)
+
+if r.status_code ==200:
+    r.raw.decode_content = True
+    with open(filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+    print('Image successfully downloaded: {}'.format(filename))
+else:
+    print("Image could\n't be retrieved")
